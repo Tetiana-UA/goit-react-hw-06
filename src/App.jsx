63a79b394859cf049695c8a4d0bfa96b25cd3341
Tetiana-UA <izidaxm85@gmail.com>
2024-03-18 13:38:33 +0100

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Переписуємо компонент на хуки
+import { useState, useEffect, useRef } from "react";
+import { useId } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ContactsForm from "./components/ContactsForm/ContactsForm";
+import ContactsList from "./components/ContactsList/ContactsList";
+import Filter from "./components/Filter/Filter";
+
+import styles from "./app.module.css";
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("my-contacts"));
+    return data || [];
+  });
+
+  const [filter, setFilter] = useState("");
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (!firstRender.current) {
+      localStorage.setItem("my-contacts", JSON.stringify(contacts));
+    }
+  }, [contacts]);
+
+  useEffect(() => {
+    firstRender.current = false;
+  }, []);
+
+  //Перевіряємо на повтори контактів при введенні
+  const isDublicate = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+    const normalizedNumber = number.toLowerCase();
+
+    const dublicate = contacts.find((item) => {
+      const normalizedCurrentName = item.name.toLowerCase();
+      const normalizedCurrentNumber = item.number.toLowerCase();
+      return (
+        normalizedCurrentName === normalizedName ||
+        normalizedCurrentNumber === normalizedNumber
+      );
+    });
+    return Boolean(dublicate);
+  };
+
+  //додаємо контакт при сабміті форми функція передається як пропс onSubmitForm  для форми Formik
+  const addContact = (data) => {
+    console.log(data);
+    if (isDublicate(data)) {
+      return alert(
+        `Contact with ${data.name} and ${data.number} already in list`
+      );
+    }
+
+    const newContact = {
+      id: useId(),
+      ...data,
+    };
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  //видадення контакту по id
+  const deleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((item) => item.id !== id)
+    );
+  };
+
+  //запис значення інпуту фільтрації
+  const changeFilter = ({ target }) => setFilter(target.value);
+
+  //фільтрація контактів за значенням фільтру
+  const getFilteredContacts = () => {
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalizedFilter = filter.toLowerCase();
+
+    const filteredContacts = contacts.filter(({ name }) => {
+      const normalizedName = name.toLowerCase();
+      return normalizedName.includes(normalizedFilter);
+    });
+    return filteredContacts;
+  };
+
+  const items = getFilteredContacts();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className={styles.wraper}>
+      <h2 className={styles.title}>PhoneBook</h2>
+      <ContactsForm onSubmitForm={addContact} />
 
-export default App
+      <h2 className={styles.title}>Contacts</h2>
+      <Filter changeFilter={changeFilter} filter={filter} />
+      <ContactsList items={items} deleteContact={deleteContact} />
+    </div>
+  );
+};
+
+export default App;
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,17 +1,19 @@
 import styles from "./contacts-list.module.css";
 
-const ContactsList = ({items,deleteContact}) => {
-    const elements=items.map(({id, name, number}) =>
-    <li 
-    className={styles.listLi} 
-    key={id}>{name}:  {number} 
-    <button className={styles.listButton} onClick={()=>deleteContact(id)} type="button">Delete</button>
-    </li>)
+const ContactsList = ({ items, deleteContact }) => {
+  const elements = items.map(({ id, name, number }) => (
+    <li className={styles.listLi} key={id}>
+      {name}: {number}
+      <button
+        className={styles.listButton}
+        onClick={() => deleteContact(id)}
+        type="button"
+      >
+        Delete
+      </button>
+    </li>
+  ));
 
-    return (
-        <ol className={styles.list}>
-            {elements}
-        </ol>
-    )
-}
+  return <ol className={styles.list}>{elements}</ol>;
+};
 export default ContactsList;
